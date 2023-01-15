f38cc3ea6350ca47adf330f225839d87715789c7
Robert Nichita <robert.nickykitta@gmail.com>
2023-01-14 19:07:59 -0500

    simpler replacement method fix

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { Client } from 'discordx';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const twitterRegex = /https:\/\/twitter\.com\/([-a-zA-Z0-9()@:%_+.~#?&//=]{0,512})/g;
+const twitterRegex = /https:\/\/twitter\.com/;
 
 const env = process.env.NODE_ENV || 'dev';
 
@@ -44,63 +44,32 @@ async function main() {
 
         bot.executeInteraction(interaction);
     });
-    
-    /*
-    * rewrites all twitter URLS in a message to fxtwitter URLs
-    * 
-    * @param MessageContent: string
-    * 
-    * @returns the message with all twitter URLs rewritten to fxtwitter URLs
-    * 
-    * e.g.
-    * "The first twitter link is https://twitter.com/LeagueOfLegends/status/1614028829823209472?s=20 
-    *  and the second one is https://twitter.com/Legoland/status/1614 
-    *  lorem https://twitter.com/ ipsum"
-    *  =>
-    *  "The first twitter link is https://fxtwitter.com/LeagueOfLegends/status/1614028829823209472?s=20 
-    *  and the second one is https://fxtwitter.com/Legoland/status/1614
-    *  lorem https://fxtwitter.com/ ipsum"
-    */
-    const rewriteTwitterUrls = (MessageContent: string) => {
-        //find all twitter URLs
-        const matches = MessageContent.matchAll(twitterRegex);
-        const arrmatches = Array.from(matches);
-        console.log(JSON.stringify(matches));
-        // this reducer will not append the part of the message past the last twitter URL
-        const partial = arrmatches.reduce((accumulator, curMatch, matchIndex) => {
-                return accumulator + 
-                /*subtract 2 from the accumulator length for each match 
-                * since we are adding 2 characters 'fx' for each match
-                * to match the correct position in the original message */
-                MessageContent.substring(accumulator.length - 2*(matchIndex), curMatch.index) + 
-                `https://fxtwitter.com/${curMatch[1]}`;
-        },"");
-        //append the rest of the message
-        return partial + MessageContent.substring(partial.length - arrmatches.length * 2,MessageContent.length);
-    };
 
     bot.on(Events.MessageCreate, (message : Message) => {     
         // ignore messages from bots
         if(message.author.bot) return;   
+
         bot.executeCommand(message);
+
         //only rewrite messages after in case it would affect commands
         const originalContent = message.content;
         const hasTwitterUrl = twitterRegex.test(originalContent);
-        console.log(`Message: ${message.content} ${(hasTwitterUrl?"has a twitter url":"has no twitter url")}`);
         if(hasTwitterUrl){
             const channel:Channel|undefined = bot.channels.cache.get(message.channelId);
             if(channel && (channel?.isTextBased() || channel?.isThread())){
-                channel.send({content: `Posted by user <@${message.author.id}>\n` + rewriteTwitterUrls(originalContent)})
-                .then((msg:Message) => {console.log(`Rewrote message with twitter link ${originalContent} to message with fxtwitter link ${msg.content}`);})
+                channel.send({content: `Posted by user <@${message.author.id}>\n` + originalContent.split('https://twitter.com').join("https://fxtwitter.com")})
+                .then((msg:Message) => {
+                    if(message.deletable){
+                        message.delete();
+                        console.log(`deleted message ${originalContent} from user ${message.author.username}`);
+                    }else{
+                        console.log(`could not delete message ${originalContent} from user ${message.author.username}`);
+                    }
+                    console.log(`Rewrote message with twitter link ${originalContent} to message with fxtwitter link ${msg.content}`);})
                 .catch(console.error);
             }
         }
-        if(message.deletable){
-            message.delete();
-            console.log(`deleted message ${originalContent} from user ${message.author.username}`);
-        }else{
-            console.log(`could not delete message ${originalContent} from user ${message.author.username}`);
-        }
+
     });
 
 
